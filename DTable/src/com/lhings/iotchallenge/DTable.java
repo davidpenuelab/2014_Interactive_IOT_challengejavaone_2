a80f44b2fc61391c5970ce039b3a78722b5ae420
davidpenuelab <davidpenuelab@gmail.com>
2014-05-28 16:01:17 +0200

    correction

--- DTable/src/com/lhings/iotchallenge/DTable.java
+++ DTable/src/com/lhings/iotchallenge/DTable.java
@@ -13,6 +13,7 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.List;
 
+import org.apache.http.NameValuePair;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
