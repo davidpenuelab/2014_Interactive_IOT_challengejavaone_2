a748ecc9a81ff2aa0d4c27e7dd08695e0d26a244
davidpenuelab <davidpenuelab@gmail.com>
2014-05-28 18:53:55 +0200

    trs

--- DTable/src/com/lhings/iotchallenge/DTable.java
+++ DTable/src/com/lhings/iotchallenge/DTable.java
@@ -285,49 +285,71 @@ public class DTable extends LhingsDevice {
     
     private void sendMessageLhings(String apikey, String uuid, String message){
         try {
-            System.out.println("1try same code as JOSE");
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            HttpPost post = new HttpPost("https://www.lhings.com/laas/api/v1/devices/" + uuid + "/");
+            post.addHeader("X-Api-Key", apikey);
+            StringEntity requestBody = new StringEntity("[{ \"name\": \"text\", \"value\": \"Hello testAPP\"}]");
+            post.setEntity(requestBody);
+            CloseableHttpResponse response = httpclient.execute(post);
+            if (response.getStatusLine().getStatusCode() != 200) {
+                System.err.println("Unable to doAction for device " + uuid + ", request failed: " + response.getStatusLine());
+                response.close();
+                System.exit(1);
+            }
+            String responseBody = EntityUtils.toString(response.getEntity());
+            response.close();
+            
+        } catch (IOException ex) {
+            ex.printStackTrace(System.err);
+            System.exit(1);
+        }
 
-			URL actionURL = new URL("https://www.lhings.com/laas/api/v1/devices/"+uuid+"/actions/viabrate");
-			HttpURLConnection conn = (HttpURLConnection) actionURL.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestMethod("PUT");
-			conn.setRequestProperty("X-Api-Key", apikey);
-			conn.setRequestProperty("Content-Type", "application/json");
-            System.out.println("2");
-            
-            JSONArray obj_call = new JSONArray();
-            
-            JSONObject obj1 = new JSONObject();
-            obj1.put("name","text");
-            obj_call.put(obj1);
-            JSONObject obj2 = new JSONObject();
-            obj2.put("value","Hello test melon");
-            obj_call.put(obj2);
-            
-//			String input = obj_call.toString();
-			String input = "[]";
-            System.out.println("3");
-            
-			OutputStream os = conn.getOutputStream();
-			os.write(input.getBytes());
-			os.flush();
-            
-            System.out.println("4");
-			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-            
-			String output;
-			System.out.println("Output from Server .... \n");
-			while ((output = br.readLine()) != null) {
-				System.out.println(output);
-			}
-            System.out.println("5");
-			conn.disconnect();
-		} catch (Exception e) {
-                // TODO Auto-generated catch block
-            System.out.println("try same code as JOSE");
-
-			e.printStackTrace();
-		}
+        
+        
+//        try {
+//            System.out.println("1try same code as JOSE");
+//
+//			URL actionURL = new URL("https://www.lhings.com/laas/api/v1/devices/"+uuid+"/actions/viabrate");
+//			HttpURLConnection conn = (HttpURLConnection) actionURL.openConnection();
+//			conn.setDoOutput(true);
+//			conn.setRequestMethod("PUT");
+//			conn.setRequestProperty("X-Api-Key", apikey);
+//			conn.setRequestProperty("Content-Type", "application/json");
+//            System.out.println("2");
+//            
+//            JSONArray obj_call = new JSONArray();
+//            
+//            JSONObject obj1 = new JSONObject();
+//            obj1.put("name","text");
+//            obj_call.put(obj1);
+//            JSONObject obj2 = new JSONObject();
+//            obj2.put("value","Hello test melon");
+//            obj_call.put(obj2);
+//            
+////			String input = obj_call.toString();
+//			String input = "[]";
+//            System.out.println("3");
+//            
+//			OutputStream os = conn.getOutputStream();
+//			os.write(input.getBytes());
+//			os.flush();
+//            
+//            System.out.println("4");
+//			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+//            
+//			String output;
+//			System.out.println("Output from Server .... \n");
+//			while ((output = br.readLine()) != null) {
+//				System.out.println(output);
+//			}
+//            System.out.println("5");
+//			conn.disconnect();
+//		} catch (Exception e) {
+//                // TODO Auto-generated catch block
+//            System.out.println("try same code as JOSE");
+//
+//			e.printStackTrace();
+//		}
         
         /*
          try {
